1310ca31c21b007f5d3d8de0a1ed6ecb1405046c
zhengzf90 <zhengzf90@163.com>
2025-08-10 15:39:21 +0800

    commit lesson5 code

---
+++ lesson5/hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@chainlink/env-enc").config()
+require("hardhat-deploy")
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy-ethers");
+
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+    mocha: {
+    timeout: 300000
+  },
+  networks: {
+    sepolia: {
+      // alchemy , infura, quicknode
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY,PRIVATE_KEY_1],
+      chainId: 11155111
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
+  },
+  namedAccounts:{
+    firstAccount:{
+      default:0
+    },
+    secondAccount:{
+      default:1
+    }
+  },
+  gasReporter:{
+    enabled: false
+  }
+};
